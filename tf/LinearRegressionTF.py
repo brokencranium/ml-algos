c34a6b79d08a585251b3d6d666d99db18df48b08
Vikas Vennavali <vvennava@vvennava008559.starbucks.net>
2019-03-17 21:37:39 -0700

    Saving model

--- tf/LinearRegressionTF.py
+++ tf/LinearRegressionTF.py
@@ -18,7 +18,8 @@ def build_graph(g):
         cost = tf.reduce_mean(tf.square(tf_y - y_hat), name='cost')
         optim = tf.train.GradientDescentOptimizer(learning_rate=0.001)
         train_op = optim.minimize(cost, name='train_op')
-    return tf_x, tf_y, cost, train_op, g
+        saver = tf.train.Saver()
+    return tf_x, tf_y, cost, train_op, g, saver
 
 
 def make_random_data():
@@ -30,7 +31,7 @@ def make_random_data():
     return x, 1.726 * x - 0.84 + np.array(y)
 
 
-def train_model(X, y_hit, cost, train_op, graph):
+def train_model(X, y_hit, cost, train_op, graph, saver):
     training_costs = []
     with tf.Session(graph=graph) as sess:
         sess.run(tf.global_variables_initializer())
@@ -41,7 +42,31 @@ def train_model(X, y_hit, cost, train_op, graph):
 
             if not e % 50:
                 print('Epoch %4d: %.4f' % (e, c))
+
+        saver.save(sess, './trained-model')
     return training_costs
+
+
+def predict(x_arr):
+    with tf.Session() as sess:
+        new_saver = tf.train.import_meta_graph('./trained-model.meta')
+        new_saver.restore(sess, './trained-model')
+        y_arr = sess.run('y_hat:0', feed_dict={'tf_x:0': x_arr})
+    return y_arr
+
+
+def plot_predictions(x_train, y_train, x_test, y_test, x_arr):
+    g2 = tf.Graph()
+    with tf.Session(graph=g2) as sess:
+        new_saver = tf.train.import_meta_graph('./trained-model.meta')
+        new_saver.restore(sess, './trained-model')
+        y_arr = sess.run('y_hat:0', feed_dict={'tf_x:0 ': x_arr})
+
+    plt.figure()
+    plt.plot(x_train, y_train, 'bo')
+    plt.plot(x_test, y_test, 'bo', alpha=0.3)
+    plt.plot(x_arr, y_arr.T[:, 0], '-r', lw=3)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -55,8 +80,11 @@ if __name__ == '__main__':
 
     n_epochs = 500
 
-    X, y_hit, cost, train_op, graph = build_graph(g)
-    training_costs = train_model(X, y_hit, cost, train_op, graph)
-
+    X, y_hit, cost, train_op, graph, saver = build_graph(g)
+    training_costs = train_model(X, y_hit, cost, train_op, graph, saver)
     plt.plot(training_costs)
     plt.show()
+
+    x_arr = np.arange(-2, 4, 0.1)
+    y_pred = predict(x_arr)
+    plot_predictions(x_train, y_train, x_test, y_test,x_arr)
